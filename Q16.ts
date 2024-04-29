e9948c1216765274578fd41ef2ce2d0cecf49961
sonia-vaswani13 <163179929+sonia-vaswani13@users.noreply.github.com>
2024-04-29 13:17:49 -0700

    Q16 ts/js node js of 45 assignment
    Q16 ts/js node js of 45 assignment

---
+++ Q16.ts
@@ -0,0 +1,25 @@
+//Q16.More Guests: You just found a bigger dinner table, so now more space is available. Think of three more guests to invite to dinner.
+//• Start with your program from Exercise 15. Add a print statement to the end of your program informing people that you found a bigger dinner table.
+
+
+let frndsArr: string[] =["Adv Haya","Adv Bushra","Adv Sidra","Adv zareena"]
+let canNOTAttend ="Adv Bushra";
+//• Add one new guest to the beginning of your array.
+let newGuest: string="Adv Sonia";
+frndsArr[frndsArr.indexOf(canNOTAttend)]=newGuest;
+console.log(frndsArr);
+frndsArr.map((item) => console.log(`Dear ${item}, I found a Bigger Dinner table`));
+//• Add one new guest to the middle of your array. 
+let guestBegin ="Adv Shanti devi";
+frndsArr.unshift(guestBegin);
+console.log(frndsArr);
+let middleGuest ="Adv sineha";
+let middleIndex =frndsArr.length / 2;
+frndsArr.splice(middleIndex, 0 ,middleGuest);
+console.log(frndsArr);
+//• Use append() to add one new guest to the end of your list.
+frndsArr.push("Dr gungun vaswani");
+console.log(frndsArr); 
+//• Print a new set of invitation messages, one for each person in your list.
+frndsArr.map((item) => (console.log(`Dear ${item}, You are Cordinally Invited to a Dinner`));
+
